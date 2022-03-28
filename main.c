7686b831a57e4bbdad843d52d682a7f9025f2180
fcho9876 <73094975+fcho9876@users.noreply.github.com>
2022-03-29 00:10:18 +1300

    Added loop capability

--- main.c
+++ main.c
@@ -5,7 +5,12 @@
 #include "functions.h"
 
 int main(void) {
-    read_user_input();
-    read_length();
-    select_password_type();
+    int user_input_loop = 1;
+    while(user_input_loop == 1) {
+        read_user_input();
+        read_length();
+        select_password_type();
+        printf("Go again? 1 = Yes, 0 = No \n");
+        scanf("%d", &user_input_loop);
+    }
 }
